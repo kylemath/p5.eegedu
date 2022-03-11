f7644e4044941e8112030a22aaeb5cf138103cd6
Kyle Mathewson <kylemath@gmail.com>
2022-03-11 08:18:41 -0700

    both pipes were going at once, may need to do the cayden copy, but for now piping both

--- src/components/PageSwitcher/components/EEGEduAnimate/EEGEduAnimate.js
+++ src/components/PageSwitcher/components/EEGEduAnimate/EEGEduAnimate.js
@@ -118,25 +118,106 @@ export function Animate(connection) {
     []
   );
 
-  const [selectedPipeline, setSelectedPipeline] = useState('bands');  
-  const handleSelectPipelineChange = useCallback((value) =>
-    {
-      setSelectedPipeline(value)
-    },
-    []
-  );
-
  // Main file in use
   const [fileContents, setFileContents] = useState();
 
+  const testScript = `
+    class MySketch extends React.Component {
+     setup(p5, whereToPlot) {
+       p5.createCanvas(500, 500).parent(whereToPlot)
+       clickValue = 0;
+     }
+
+     draw(p5) {
+       p5.background(clickValue,255,200);
+
+       // You can set some useful variables
+       // to use more often like this:
+       // Notice how everything starts with p5.
+       HEIGHT = p5.height
+       WIDTH = p5.width;
+       MOUSEX = p5.mouseX;
+       MOUSEY = p5.mouseY;
+
+       // Availalable EEG Variables:
+       // Electrodes 0, 1, 2, 3
+       // Delta, Theta, Alpha, Beta, Gamma
+       // e.g.:
+       DELTA = brain.current.bands.data.delta[0] * 10;
+       THETA = brain.current.bands.data.theta[0]* 10;
+       ALPHA = brain.current.bands.data.alpha[0]* 10;   
+       BETA =  brain.current.bands.data.beta[0]* 10;
+       GAMMA =  brain.current.bands.data.gamma[0]* 10;
+       
+       gap = (DELTA + THETA + ALPHA + BETA + GAMMA)/5;
+       
+       p5.textSize(DELTA);
+       p5.fill(clickValue,DELTA*5,20);
+       p5.text('Delta', MOUSEX+40, MOUSEY-(2*gap));
+       p5.ellipse(MOUSEX,MOUSEY-(2*gap),DELTA);
+
+       p5.textSize(THETA);
+       p5.fill(20, clickValue, THETA*5);
+       p5.text('Theta', MOUSEX+40, MOUSEY-(1*gap));
+       p5.ellipse(MOUSEX,MOUSEY-(1*gap),THETA);
+
+       p5.textSize(ALPHA);
+       p5.fill(ALPHA*5, clickValue, clickValue);
+       p5.text('Alpha', MOUSEX+40, MOUSEY);   
+       p5.ellipse(MOUSEX,MOUSEY,ALPHA);
+
+       p5.textSize(BETA);
+       p5.fill(BETA*5, clickValue, BETA*5);
+       p5.text('Beta', MOUSEX+40, MOUSEY+(1*gap));   
+       p5.ellipse(MOUSEX,MOUSEY+(1*gap),BETA);
+
+       p5.textSize(GAMMA);
+       p5.fill(clickValue, GAMMA*5/2, GAMMA*5);
+       p5.text('Gamma', MOUSEX+40, MOUSEY+(2*gap));   
+       p5.ellipse(MOUSEX,MOUSEY+(2*gap),GAMMA);
+          
+     }
+
+     // other p5 functions can be created like this
+     // but must be included below in the return call
+     mouseClicked(p5) {
+       p5.background(200,255,210)
+       if (clickValue === 0) {
+         clickValue = 255;
+         } else {
+         clickValue = 0;
+         }
+     }
+
+     render() {
+       return (
+          <Sketch
+            setup={this.setup}
+            draw={this.draw}
+            mouseClicked={this.mouseClicked}
+          />
+       )
+     }
+
+    }
+
+    render (
+     <MySketch />
+    )
+  `
+
   // Load in file for first time
   useEffect(()=>{
-    readFile(pathPrefix + 'BasicFrequencyBands.p5')
+    // readFile(pathPrefix + 'BasicFrequencyBands.p5')
+    setFileContents(testScript)
   }, []) // <-- empty dependency array
 
+  const bandsProto = {delta:[0], theta:[0], alpha:[0], beta:[0], gamma:[0], textMsg: "No data."}
+  const spectraProto = {psd:[0], freqs:[0], textMsg: "No data."}
+
   const brain = useRef({
-    data: {delta:[0], theta:[0], alpha:[0], beta:[0], gamma:[0], psd:[0], freqs:[0], info:' '},
-    textMsg: "No data.",
+    bands: bandsProto,
+    spectra: spectraProto,
   });
 
   // Wrap this whole thing in useEffect to control when it updates
@@ -147,6 +228,7 @@ export function Animate(connection) {
     let pipeBands$;
     let pipeSpectra$
     let multicastBands$;
+    let multicastSpectra$;
     let museClient;   
 
     const connectMuse = async() => {
@@ -166,70 +248,78 @@ export function Animate(connection) {
           channelData$ = museClient.eegReadings;
         }
 
-        pipeBands$ = zipSamples(channelData$).pipe(
-          bandpassFilter({
-            cutoffFrequencies: [
-              animateSettings.cutOffLow,
-              animateSettings.cutOffHigh,
-            ],
-            nbChannels: animateSettings.nbChannels,
-          }),
-          epoch({
-            duration: animateSettings.duration,
-            interval: animateSettings.interval,
-            samplingRate: animateSettings.srate,
-          }),
-          fft({ bins: animateSettings.bins }),
-          powerByBand(),
-          catchError((err) => {
-            console.log(err);
-          })
-        );
 
-        pipeSpectra$ = zipSamples(channelData$).pipe(
-          bandpassFilter({
-            cutoffFrequencies: [
-              animateSettings.cutOffLow,
-              animateSettings.cutOffHigh,
-            ],
-            nbChannels: animateSettings.nbChannels,
-          }),
-          epoch({
-            duration: animateSettings.duration,
-            interval: animateSettings.interval,
-            samplingRate: animateSettings.srate,
-          }),
-          fft({ bins: animateSettings.bins }),
-          sliceFFT([1, 128]),
-          catchError((err) => {
-            console.log(err);
-          })
-        );    
-
-        if (selectedPipeline === 'bands') {
-
+          pipeBands$ = zipSamples(channelData$).pipe(
+            bandpassFilter({
+              cutoffFrequencies: [
+                animateSettings.cutOffLow,
+                animateSettings.cutOffHigh,
+              ],
+              nbChannels: animateSettings.nbChannels,
+            }),
+            epoch({
+              duration: animateSettings.duration,
+              interval: animateSettings.interval,
+              samplingRate: animateSettings.srate,
+            }),
+            fft({ bins: animateSettings.bins }),
+            powerByBand(),
+            catchError((err) => {
+              console.log(err);
+            })
+          );          
           multicastBands$ = pipeBands$.pipe(multicast(() => new Subject()));
+          multicastBands$.subscribe((data) => {
+            brain.current.bands = {
+              data: data,
+              textMsg: "Data received",
+            }; 
+            console.log('brain output: ', brain.current)
 
-        } else if (selectedPipeline === 'spectra') {
+          });
 
-          multicastBands$ = pipeSpectra$.pipe(multicast(() => new Subject()));
-      
-        }
+          multicastBands$.connect();
 
-        multicastBands$.subscribe((data) => {
-          brain.current = {
-            data: data,
-            textMsg: "Data received",
-          }; 
-        });
+ 
+          pipeSpectra$ = zipSamples(channelData$).pipe(
+            bandpassFilter({
+              cutoffFrequencies: [
+                animateSettings.cutOffLow,
+                animateSettings.cutOffHigh,
+              ],
+              nbChannels: animateSettings.nbChannels,
+            }),
+            epoch({
+              duration: animateSettings.duration,
+              interval: animateSettings.interval,
+              samplingRate: animateSettings.srate,
+            }),
+            fft({ bins: animateSettings.bins }),
+            sliceFFT([1, 128]),
+            catchError((err) => {
+              console.log(err);
+            })
+          );
 
-        multicastBands$.connect();
+          multicastSpectra$ = pipeSpectra$.pipe(multicast(() => new Subject()));
+          multicastSpectra$.subscribe((data) => {
+            brain.current.spectra = {
+              data: data,
+              textMsg: "Data received",
+            }; 
+            console.log('brain output: ', brain.current)
+
+          });
+
+          multicastSpectra$.connect();      
+
+
       }
     }
 
     buildBrain();
 
-  }, [connection, selectedPipeline])
+  }, [connection])
 
   function renderEditor() {
     const scope = { styled, brain, React, Sketch };
@@ -240,13 +330,7 @@ export function Animate(connection) {
           scope={scope}
           noInline={true}
           theme={theme}
-        >
-        <Select
-            label="Select Pipeline"
-            options={['bands', 'spectra']}
-            onChange={handleSelectPipelineChange}
-            value={selectedPipeline}
-          />     
+        >  
          <Select
             label="Select Example Code"
             options={options}
