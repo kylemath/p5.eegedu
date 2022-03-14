b63470cdda4964832d6436fc6f8b30a949240d61
Kyle Mathewson <kylemath@gmail.com>
2022-03-13 20:13:09 -0600

    working multiple threads of data

--- src/components/PageSwitcher/components/EEGEduAnimate/EEGEduAnimate.js
+++ src/components/PageSwitcher/components/EEGEduAnimate/EEGEduAnimate.js
@@ -9,11 +9,10 @@ import {
   Select,
   ButtonGroup,
   Link,
+  List,
 } from "@shopify/polaris";
 import { zipSamples, MuseClient } from "muse-js";
-import { bandpassFilter, epoch, fft, powerByBand, sliceFFT } from "@neurosity/pipes";
-import { catchError, multicast } from "rxjs/operators";
-import { Subject } from "rxjs";
+import { bandpassFilter, epoch, fft, powerByBand } from "@neurosity/pipes";
 import Sketch from "react-p5";
 import styled from "styled-components";
 import { LiveProvider, LiveEditor, LiveError, LivePreview } from "react-live";
@@ -22,6 +21,7 @@ import { saveAs } from "file-saver";
 import { mockMuseEEG } from "../../utils/mockMuseEEG";
 import { chartStyles } from "../chartOptions";
 import theme from "./p5Theme";
+import {protos} from "./utils";
 
 const animateSettings = {
   cutOffLow: .1,
@@ -33,9 +33,26 @@ const animateSettings = {
   srate: 256,
 };
 
+const pathPrefix = 'https://raw.githubusercontent.com/kylemath/p5.eegedu.art/allInOne/';
+const address = 'https://api.github.com/repos/kylemath/p5.eegedu.art/git/trees/allInOne?recursive=1';
+
 export function Animate(connection) {
 
+  // Output Prototypes
+  // --------------
+  const brain = useRef({
+    epochs: protos.epochs,
+    bands: protos.bands,
+    spectra: protos.spectra,
+  });
+
+  // Main file in use
+  // ------------------
+  const [fileContents, setFileContents] = useState();
+
+
   // Populate Select file list from github repo .art
+  //------------------------
 
   function readRepoList(value) {
     function reqListener () {
@@ -45,34 +62,58 @@ export function Animate(connection) {
     oReq.addEventListener("load", reqListener);
     oReq.open("GET", value);
     oReq.send();
+
   }
 
-  const [repoContents, setRepoContents] = useState();
-
-  const pathPrefix = 'https://raw.githubusercontent.com/kylemath/p5.eegedu.art/main/';
-  const address = 'https://api.github.com/repos/kylemath/p5.eegedu.art/git/trees/main?recursive=1';
   let options = [];
-
+  const [repoContents, setRepoContents] = useState();
   useEffect(()=>{
+    console.log('Reading Repo List')
     readRepoList(address)
   }, []) // <-- empty dependency array
 
   if (repoContents) {
+    console.log('Parsing repo list');
     const repoObj = JSON.parse(repoContents)
 
     for (let i = 0; i < repoObj.tree.length; i++) {
-        if (repoObj.tree[i].path.charAt(repoObj.tree[i].path.length-1) === '5') 
-        {
-          options.push({
-            label: repoObj.tree[i].path, 
-            value: pathPrefix + repoObj.tree[i].path
-          })
-        }
+      if (repoObj.tree[i].path.charAt(repoObj.tree[i].path.length-1) === '5') 
+      {
+        options.push({
+          label: repoObj.tree[i].path, 
+          value: pathPrefix + repoObj.tree[i].path
+        })
+      }
     }
-
   }
 
-  //Uploading file
+  // Read file from web
+  // -------------------
+
+  function readFile(value) {
+    console.log('reading file')
+    function reqListener () {
+      setFileContents(this.responseText);
+    }
+    var oReq = new XMLHttpRequest();
+    oReq.addEventListener("load", reqListener);
+    oReq.open("GET", value);
+    oReq.send();
+  }
+
+  const [selectedWebCode, setSelectedWebCode] = useState(pathPrefix + 'BasicFrequencyBands.p5');  
+  const handleSelectWebCodeChange = useCallback((value) =>
+    {
+      console.log('Reading Selected Web file')
+      setSelectedWebCode(value)
+      readFile(value)
+    },
+    []
+  );
+
+  // Uploading files
+  //---------------------
+
   const [uploadFile, setUploadFile] = useState();
   const handleDropZoneDrop = useCallback(
     (_dropFiles, acceptedFiles, _rejectedFiles) => {
@@ -90,149 +131,38 @@ export function Animate(connection) {
     </Stack>
   );
 
-  // Saving File
+  // Saving Files
+  // ---------------------
+
   const [fileName, setFileName] = useState("MySketch.p5");
   const handleFilenameChange = useCallback(
     (newValue) => setFileName(newValue),
     []
   );
 
-  // Read file from web
-  function readFile(value) {
-    console.log('reading file')
-    function reqListener () {
-      setFileContents(this.responseText);
-    }
-    var oReq = new XMLHttpRequest();
-    oReq.addEventListener("load", reqListener);
-    oReq.open("GET", value);
-    oReq.send();
-  }
-
-  const [selectedWebCode, setSelectedWebCode] = useState(pathPrefix + 'BasicFrequencyBands.p5');  
-  const handleSelectWebCodeChange = useCallback((value) =>
-    {
-      setSelectedWebCode(value)
-      readFile(value)
-    },
-    []
-  );
-
- // Main file in use
-  const [fileContents, setFileContents] = useState();
-
-  const testScript = `
-    class MySketch extends React.Component {
-     setup(p5, whereToPlot) {
-       p5.createCanvas(500, 500).parent(whereToPlot)
-       clickValue = 0;
-     }
-
-     draw(p5) {
-       p5.background(clickValue,255,200);
-
-       // You can set some useful variables
-       // to use more often like this:
-       // Notice how everything starts with p5.
-       HEIGHT = p5.height
-       WIDTH = p5.width;
-       MOUSEX = p5.mouseX;
-       MOUSEY = p5.mouseY;
-
-       // Availalable EEG Variables:
-       // Electrodes 0, 1, 2, 3
-       // Delta, Theta, Alpha, Beta, Gamma
-       // e.g.:
-       DELTA = brain.current.bands.data.delta[0] * 10;
-       THETA = brain.current.bands.data.theta[0]* 10;
-       ALPHA = brain.current.bands.data.alpha[0]* 10;   
-       BETA =  brain.current.bands.data.beta[0]* 10;
-       GAMMA =  brain.current.bands.data.gamma[0]* 10;
-       
-       gap = (DELTA + THETA + ALPHA + BETA + GAMMA)/5;
-       
-       p5.textSize(DELTA);
-       p5.fill(clickValue,DELTA*5,20);
-       p5.text('Delta', MOUSEX+40, MOUSEY-(2*gap));
-       p5.ellipse(MOUSEX,MOUSEY-(2*gap),DELTA);
-
-       p5.textSize(THETA);
-       p5.fill(20, clickValue, THETA*5);
-       p5.text('Theta', MOUSEX+40, MOUSEY-(1*gap));
-       p5.ellipse(MOUSEX,MOUSEY-(1*gap),THETA);
-
-       p5.textSize(ALPHA);
-       p5.fill(ALPHA*5, clickValue, clickValue);
-       p5.text('Alpha', MOUSEX+40, MOUSEY);   
-       p5.ellipse(MOUSEX,MOUSEY,ALPHA);
-
-       p5.textSize(BETA);
-       p5.fill(BETA*5, clickValue, BETA*5);
-       p5.text('Beta', MOUSEX+40, MOUSEY+(1*gap));   
-       p5.ellipse(MOUSEX,MOUSEY+(1*gap),BETA);
-
-       p5.textSize(GAMMA);
-       p5.fill(clickValue, GAMMA*5/2, GAMMA*5);
-       p5.text('Gamma', MOUSEX+40, MOUSEY+(2*gap));   
-       p5.ellipse(MOUSEX,MOUSEY+(2*gap),GAMMA);
-          
-     }
-
-     // other p5 functions can be created like this
-     // but must be included below in the return call
-     mouseClicked(p5) {
-       p5.background(200,255,210)
-       if (clickValue === 0) {
-         clickValue = 255;
-         } else {
-         clickValue = 0;
-         }
-     }
-
-     render() {
-       return (
-          <Sketch
-            setup={this.setup}
-            draw={this.draw}
-            mouseClicked={this.mouseClicked}
-          />
-       )
-     }
-
-    }
-
-    render (
-     <MySketch />
-    )
-  `
-
-  // Load in file for first time
+  // Load file once (not when connection updates like useEffect below)
+  // -----------------------
+  
   useEffect(()=>{
-    // readFile(pathPrefix + 'BasicFrequencyBands.p5')
-    setFileContents(testScript)
+   console.log('Loading in Test File or first file');
+    readFile(pathPrefix + 'BasicFrequencyBands.p5')
+    // setFileContents(testScript)
+
   }, []) // <-- empty dependency array
 
-  const bandsProto = {delta:[0], theta:[0], alpha:[0], beta:[0], gamma:[0], textMsg: "No data."}
-  const spectraProto = {psd:[0], freqs:[0], textMsg: "No data."}
-
-  const brain = useRef({
-    bands: bandsProto,
-    spectra: spectraProto,
-  });
 
   // Wrap this whole thing in useEffect to control when it updates
   // it only updates when dependencies are changed, which in this case is just [connection]
   useEffect(() => {
-
+ 
     let channelData$;
-    let pipeBands$;
+    let pipeEpochs$;
     let pipeSpectra$
-    let multicastBands$;
-    let multicastSpectra$;
+    let pipeBands$;
+
     let museClient;   
 
     const connectMuse = async() => {
-      console.log('dingdong')
       museClient = new MuseClient();
       await museClient.connect();
       await museClient.start();
@@ -242,12 +172,68 @@ export function Animate(connection) {
     const buildBrain = async() => {
       if (connection.status.connected) {
         if (connection.status.type === "mock") {
+          console.log('Connecting to mock data');
           channelData$ = mockMuseEEG(256);
         } else {
+          console.log('connecting to muse data');
           await connectMuse();
           channelData$ = museClient.eegReadings;
         }
 
+          // ------
+          // epochs
+          //------
+          pipeEpochs$ = zipSamples(channelData$).pipe(
+            bandpassFilter({
+              cutoffFrequencies: [
+                animateSettings.cutOffLow,
+                animateSettings.cutOffHigh,
+              ],
+              nbChannels: animateSettings.nbChannels,
+            }),
+            epoch({
+              duration: animateSettings.duration,
+              interval: animateSettings.interval,
+              samplingRate: animateSettings.srate,
+            })
+          );
+          pipeEpochs$.subscribe((dataEpoch) => {
+            brain.current.epochs = {
+              data: dataEpoch,
+              dataReceived: true,
+            };
+           });
+     
+
+          // ------
+          // spectra
+          //------  
+
+          pipeSpectra$ = zipSamples(channelData$).pipe(
+            bandpassFilter({
+              cutoffFrequencies: [
+                animateSettings.cutOffLow,
+                animateSettings.cutOffHigh,
+              ],
+              nbChannels: animateSettings.nbChannels,
+            }),
+            epoch({
+              duration: animateSettings.duration,
+              interval: animateSettings.interval,
+              samplingRate: animateSettings.srate,
+            }),
+            fft({ bins: animateSettings.bins })
+          );
+          pipeSpectra$.subscribe((dataSpectra) => {
+            brain.current.spectra = {
+              data: dataSpectra,
+              dataReceived: true,
+            }; 
+          });
+
+          // ------
+          // bands
+          //------
 
           pipeBands$ = zipSamples(channelData$).pipe(
             bandpassFilter({
@@ -263,56 +249,14 @@ export function Animate(connection) {
               samplingRate: animateSettings.srate,
             }),
             fft({ bins: animateSettings.bins }),
-            powerByBand(),
-            catchError((err) => {
-              console.log(err);
-            })
+            powerByBand()
           );          
-          multicastBands$ = pipeBands$.pipe(multicast(() => new Subject()));
-          multicastBands$.subscribe((data) => {
+          pipeBands$.subscribe((dataBands) => {
             brain.current.bands = {
-              data: data,
-              textMsg: "Data received",
+              data: dataBands,
+              dataReceived: true,
             }; 
-            console.log('brain output: ', brain.current)
-
           });
-
-          multicastBands$.connect();
-
- 
-          pipeSpectra$ = zipSamples(channelData$).pipe(
-            bandpassFilter({
-              cutoffFrequencies: [
-                animateSettings.cutOffLow,
-                animateSettings.cutOffHigh,
-              ],
-              nbChannels: animateSettings.nbChannels,
-            }),
-            epoch({
-              duration: animateSettings.duration,
-              interval: animateSettings.interval,
-              samplingRate: animateSettings.srate,
-            }),
-            fft({ bins: animateSettings.bins }),
-            sliceFFT([1, 128]),
-            catchError((err) => {
-              console.log(err);
-            })
-          );
-
-          multicastSpectra$ = pipeSpectra$.pipe(multicast(() => new Subject()));
-          multicastSpectra$.subscribe((data) => {
-            brain.current.spectra = {
-              data: data,
-              textMsg: "Data received",
-            }; 
-            console.log('brain output: ', brain.current)
-
-          });
-
-          multicastSpectra$.connect();      
-
 
       }
     }
@@ -323,6 +267,23 @@ export function Animate(connection) {
 
   function renderEditor() {
     const scope = { styled, brain, React, Sketch };
+
+    function saveFile() {
+      const text = document.getElementById("liveEditor").firstChild.value;
+      const blob = new Blob([text], { type: "text/plain;charset=utf-8" });
+      saveAs(blob, fileName );
+    }
+    function loadFile() {
+      uploadFile.text().then((content) => setFileContents(content));
+    }
+
+    function resetSelectedCode() {
+      readFile(selectedWebCode)
+    }
+    function restartCurrentCode() {
+      const text = document.getElementById("liveEditor").firstChild.value
+      setFileContents(text.concat(' '));
+    }  
 
     return (  
         <LiveProvider
@@ -337,14 +298,10 @@ export function Animate(connection) {
             onChange={handleSelectWebCodeChange}
             value={selectedWebCode}
           />      
-
           <br />
-
           <LiveEditor id="liveEditor" />
             {fileContents && <LiveError />}
-
-
-          {fileContents && <LivePreview />} 
+          {connection.status.connected && fileContents && <LivePreview />} 
           <ButtonGroup>
             <Button
               onClick={() => {
@@ -410,40 +367,12 @@ export function Animate(connection) {
 
           </Card.Section>
         </LiveProvider>
-
     );
-
-    function saveFile() {
-      const text = document.getElementById("liveEditor").firstChild.value;
-      const blob = new Blob([text], { type: "text/plain;charset=utf-8" });
-      saveAs(blob, fileName );
-    }
-    function loadFile() {
-      uploadFile.text().then((content) => setFileContents(content));
-    }
-
-    function resetSelectedCode() {
-      readFile(selectedWebCode)
-    }
-    function restartCurrentCode() {
-      const text = document.getElementById("liveEditor").firstChild.value
-      setFileContents(text.concat(' '));
-    }  
   }
 
   return (
     <Card title="Animate your brain waves"> 
       <Card.Section>
-        <p>
-        {[
-          'The live animation is controlled by the P5.js code below. The code is ',
-          'editable. Play around with the numbers and see what happens. The',
-          'brain.current variables are only available if there is a data source',
-          'connected. EEG bands and locations are available by calling ' ,
-          'brain.current.data.alpha[0], for example for the left ear electrode alpha.',
-        ]}
-        </p>
-        <br />
         <p>
           {
             'The examples below are all stored in a '
@@ -455,7 +384,25 @@ export function Animate(connection) {
           '. You can save your .p5 code with a button at the bottom of the page.',
           'You can then submit your artwork as code to the repository.',
           ]}
+        </p>   
+        <br />   
+        <p>
+        {[
+          'The live animation is controlled by the P5.js code below. The code is ',
+          'editable. Play around with the numbers and see what happens. The',
+          'brain.current variables are only available if there is a data source',
+          'connected. EEG bands and locations are available by calling: ' ,
+        ]}
         </p>
+        <br />
+        <List type="bullet">
+          <List.Item><i>brain.current.bands.data.alpha[0] </i>- Left Ear Alpha Power.</List.Item>
+          <List.Item><i>brain.current.bands.data.theta[3]  </i>- Right Ear Theta Power.</List.Item>
+          <List.Item><i>brain.current.spectra.data.psd[0]  </i>- Left Ear frequency Spectra.</List.Item>
+          <List.Item><i>brain.current.spectra.data.freqs  </i>- corresponding frequencies.</List.Item>
+          <List.Item><i>brain.current.epochs.data.data[2]  </i>- Right Forehead Epoched time series filtered data</List.Item>
+          <List.Item><i>brain.current.epochs.data.info.samplingRate  </i>- data sampling rate.</List.Item>
+        </List>
       </Card.Section>
       <Card.Section>
         <div style={chartStyles.wrapperStyle.style}>{renderEditor()}</div>
